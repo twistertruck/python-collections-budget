c8e1d91fdf3f687670e6e4cbba1a16691bc08e23
twistertruck <95315065+twistertruck@users.noreply.github.com>
2021-12-09 18:12:11 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,11 +1,2 @@
-from _typeshed import Self
-
-
 class BudgetList:
-    def __init__(Self, budget)
-    Self.budget = budget
-    Self.sum_expenses = 0
-    Self.expenses = []
-    Self.sum_overages = 0
-    Self.overages = []
-
+    pass
